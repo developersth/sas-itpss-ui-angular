a5296696735301990246f3ec52b69c542382cb46
developersth <kritsadee.satewin@gmail.com>
2024-01-18 01:02:04 +0700

    update docs-modal form

--- src/app/components/dmss/docs-modal/docs-modal.component.ts
+++ src/app/components/dmss/docs-modal/docs-modal.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { NgbDateStruct } from "@ng-bootstrap/ng-bootstrap";
+import {
+  FormBuilder,
+  FormGroup,
+  UntypedFormControl,
+  UntypedFormGroup,
+  Validators,
+} from "@angular/forms";
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 @Component({
   selector: "app-docs-modal",
   templateUrl: "./docs-modal.component.html",
@@ -9,19 +17,47 @@ import { NgbDateStruct } from "@ng-bootstrap/ng-bootstrap";
   ],
 })
 export class DocsModalComponent implements OnInit {
-  constructor() {}
-  dtDocDate: NgbDateStruct;
+  constructor(public formBuilder: FormBuilder,) {
+    this.docForm = this.formBuilder.group({
+      statusId: [0]
+    });
+  }
+  dtStart: NgbDateStruct;
+  dtEnd:NgbDateStruct;
   now: Date = new Date();
+  status:any=[];
+  docForm:FormGroup;
   ngOnInit(): void {
+
     this.selectToday();
     this.now = new Date();
+    this.getStatus();
+    this.buildItemForm();
+  }
+  private buildItemForm() {
+    this.docForm = this.formBuilder.group({
+      statusId: [0]
+    });
+  }
+  onSubmit() {
+  }
+  getStatus(){
+    return this.status=[
+      {statusId:1,statusName:'Sending PO'},
+      {statusId:2,statusName:'Order Acknowleged'},
+      {statusId:3,statusName:'Ready to Shipped'},
+      {statusId:4,statusName:'On the Way'},
+      {statusId:5,statusName:'Clearance'},
+      {statusId:6,statusName:'Received'},
+      {statusId:7,statusName:'Completed'},
+    ]
   }
   isDisabled(date: NgbDateStruct, current: { month: number }) {
     return date.month !== current.month;
   }
   // Selects today's date
   selectToday() {
-    this.dtDocDate = {
+    this.dtStart = {
       year: this.now.getFullYear(),
       month: this.now.getMonth() + 1,
       day: this.now.getDate(),
@@ -33,4 +69,5 @@ export class DocsModalComponent implements OnInit {
     const d = new Date(date.year, date.month - 1, date.day);
     return d.getDay() === 0 || d.getDay() === 6;
   }
+
 }
